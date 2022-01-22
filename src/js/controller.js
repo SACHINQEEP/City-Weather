2ddde405dbb1380a16ca09baa832e5f8b73d06f4
SACHINQEEP <patelsachinsp269@gmail.com>
2022-01-23 00:49:09 +0530

    main window

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,13 @@ import * as model from './model.js';
 import Rendercoords from './view/coords.js';
 import renderWeather from './view/weatherView.js';
 import search from './view/searchView.js';
+import renderMainView from './view/mainView.js';
+
+const controlMain = function () {
+  renderMainView.render();
+};
+
+controlMain();
 
 const controlData = async function () {
   try {
