b229f667cfb2ac847f4377d23e8eb738a2d8387e
SACHINQEEP <patelsachinsp269@gmail.com>
2022-01-23 02:37:15 +0530

    icon

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,4 @@ export const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 export const TEMP_UNIT = '&units=metric';
 export const WEATHER_ICONS = 'http://openweathermap.org/img/wn/';
 export const ICONS_SIZE = '@4x.png';
-export const TIMEOUT_SEC = 10;
+export const TIMEOUT_SEC = 9;
